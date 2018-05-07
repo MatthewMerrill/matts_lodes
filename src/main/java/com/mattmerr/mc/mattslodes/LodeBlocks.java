6d91b92b28c1772a0a91b65afdb372d64f4d18eb
Matthew Merrill <MattMerr47@gmail.com>
2018-05-06 23:15:53 -0700

    textures and things

--- src/main/java/com/mattmerr/mc/mattslodes/LodeBlocks.java
+++ src/main/java/com/mattmerr/mc/mattslodes/LodeBlocks.java
@@ -1,33 +1,47 @@
 package com.mattmerr.mc.mattslodes;
 
 import com.mattmerr.mc.mattslodes.block.IronLode.IronLodeBlock;
-import com.mattmerr.mc.mattslodes.block.IronLode.IronLodeItem;
+
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(modid = MattsLodesMod.MODID)
 public class LodeBlocks {
 
   public static final IronLodeBlock ironLodeBlock = new IronLodeBlock();
-  public static final IronLodeItem ironLodeItem = new IronLodeItem();
+  public static final Item ironLodeItem = new ItemBlock(ironLodeBlock)
+      .setRegistryName("iron_lode");
 
   @SubscribeEvent
   public static void registerBlocks(RegistryEvent.Register<Block> event) {
     event.getRegistry().registerAll(ironLodeBlock);
-    MattsLodesMod.logger.info("lode blocks registered");
+    MattsLodesMod.logger.info("lode block registered");
   }
 
   @SubscribeEvent
   public static void registerItems(RegistryEvent.Register<Item> event) {
-    event.getRegistry().registerAll(ironLodeItem);
+    event.getRegistry().register(ironLodeItem);
     MattsLodesMod.logger.info("lode items registered");
   }
 
+  @SubscribeEvent
+  public static void registerRenders(ModelRegistryEvent event) {
+    registerRender(ironLodeItem);
+  }
 
+  private static void registerRender(Item item) {
+    ModelLoader.setCustomModelResourceLocation(item, 0,
+        new ModelResourceLocation(item.getRegistryName(), "inventory"));
+  }
 
 
 }
--- src/main/java/com/mattmerr/mc/mattslodes/block/IronLode.java
+++ src/main/java/com/mattmerr/mc/mattslodes/block/IronLode.java
@@ -1,22 +1,32 @@
 package com.mattmerr.mc.mattslodes.block;
 
 import com.mattmerr.mc.mattslodes.LodeBlocks;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber
 public class IronLode {
 
   public static class IronLodeBlock extends Block {
 
     public IronLodeBlock() {
       super(Material.IRON, MapColor.IRON);
-      setUnlocalizedName("ironlode");
-      setRegistryName("ironlode");
-      setHardness(2000);
+      setRegistryName("iron_lode");
+      setUnlocalizedName("iron_lode");
+      setHardness(1);
 //      setResistance(1.0); // TODO: IDK
       setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
     }
@@ -27,11 +37,26 @@ public class IronLode {
 
     public IronLodeItem() {
       super(LodeBlocks.ironLodeBlock);
-      setRegistryName("ironlode");
-      setUnlocalizedName("ironlode");
+      setRegistryName("iron_lode");
+      setUnlocalizedName("iron_lode");
       setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
       setMaxStackSize(1);
-
     }
   }
+
+  @SubscribeEvent
+  public static void blockBreak(BlockEvent.BreakEvent breakEvent) {
+    if (breakEvent.getState().getBlock() == LodeBlocks.ironLodeBlock) {
+      World w = breakEvent.getWorld();
+      BlockPos pos = breakEvent.getPos();
+
+      EntityItem item = new EntityItem(w,
+          pos.getX(), pos.getY(), pos.getZ(),
+          new ItemStack(Blocks.IRON_ORE, 1));
+
+      w.spawnEntity(item);
+      breakEvent.setCanceled(true);
+    }
+  }
+
 }
